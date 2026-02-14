7a54dd078f0daac568da58e0578e0d5e0f4a4626
Tiffany Calle <tiffanycalle@Tiffanys-MBP-2.lan>
2026-02-13 20:10:12 -0500

    exception handling

--- parser.py
+++ parser.py
@@ -26,3 +26,22 @@ def find_dates(text):
         found_dates.extend(matches)
         
     return found_dates
+
+# read from file made in reader.py
+try:
+    with open("collected_info.txt", "r") as f:
+        raw_text = f.read()
+    
+    dates = find_dates(raw_text)
+    
+    #dates found
+    if dates:
+        print(f"I found {len(dates)} potential dates:")
+        for date in dates:
+            print(f" - {date}")
+    else:
+        print("I couldn't find any dates. The format might be different. Take a closer look.")
+
+# exception handling
+except FileNotFoundError:
+    print("Error: I can't find 'collected_info.txt'. Did you run reader.py first?")
